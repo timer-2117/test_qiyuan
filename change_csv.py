cbc6050324960dbc93ed86561a53b7f28745e3b6
timer-2117 <1127311411@qq.com>
2023-06-14 16:00:14 +0800

    support chunk

--- change_csv.py
+++ change_csv.py
@@ -1,17 +1,18 @@
-import pandas as pd			#pandas是一个强大的分析结构化数据的工具集
+import pandas as pd                     #pandas是一个强大的分析结构化数据的工具集
 
 
-read_path='./import/large_vertex.csv'
-write_path='./import/large_vertex_final.csv'
+read_path='./import/gsh-2015-edge.csv'
+write_path='./import/gsh-2015-edge_1.csv'
 # 将csv文件内数据读出
-ngData=pd.read_csv(read_path)
+ngData=pd.read_csv(read_path,chunksize=1000000)
 
 #添加新列‘名字长度’（length）
-ngList=[]                                       			#准备一个列表，把新列的数据存入其中
-for index,row in ngData.iterrows():			                #遍历数据表，计算每一位名字的长度
-    ngList.append('person')
-ngData[':LABEL']=ngList                                     #注明列名，就可以直接添加新列
-ngData.to_csv(write_path,index=False)         #把数据写入数据集，index=False表示不加索引
+for chunk in ngData:                                    #遍历数据表，计算每一位名字的长度
+    ngList=[]
+    for i in range(len(chunk)):
+        ngList.append('knows')
+    chunk['knows']=ngList                                     #注明列名，就可以直接添加新列
+    chunk.to_csv(write_path,index=False,chunksize=1000000)         #把数据写入数据集，index=False表示不加索引
 #注意这里的ngData['length']=ngList是直接在原有数据基础上加了一列新的数据，也就是说现在的ngData已经具备完整的3列数据
 #不用再在to_csv中加mode=‘a’这个参数，实现不覆盖添加。
 
